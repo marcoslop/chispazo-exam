62666939003d2df10acea9ee1fb5a4602e890a38
marcoslop <marcos.lopez.miguel@gmail.com>
2017-02-13 10:19:35 +0100

    Added swagger auto doc.

--- src/main/java/com/tulotero/games/chispazo/rest/ChispazoResource.java
+++ src/main/java/com/tulotero/games/chispazo/rest/ChispazoResource.java
@@ -1,9 +1,14 @@
 package com.tulotero.games.chispazo.rest;
 
+import com.tulotero.games.beans.PrizeInfo;
 import com.tulotero.games.chispazo.ChispazoDrawService;
 import com.tulotero.games.chispazo.ChispazoPrizeCalculator;
 import com.tulotero.games.chispazo.beans.ChispazoBet;
 import com.tulotero.games.chispazo.beans.ChispazoDraw;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -13,6 +18,7 @@ import java.util.Optional;
 
 @Path("/chispazo")
 @Produces(MediaType.APPLICATION_JSON)
+@Api(value = "/Chispazo", description = "Chispazo Game API")
 public class ChispazoResource {
 
     @Inject
@@ -23,6 +29,13 @@ public class ChispazoResource {
 
     @Path("/draws/{drawId}")
     @GET
+    @ApiOperation(value = "Returns information about a Chispazo Draw",
+            response = ChispazoDraw.class
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok. Information with the Chispazo Draw"),
+            @ApiResponse(code = 404, message = "No Chispazo found for the Id passed as argument"),
+    })
     public Response getDrawInfo(@PathParam("drawId") Long drawId){
         Optional<ChispazoDraw> drawOpt = drawService.getDraw(drawId);
         if (drawOpt.isPresent()){
@@ -37,6 +50,14 @@ public class ChispazoResource {
 
     @Path("/draws/{drawId}/prize")
     @POST
+    @ApiOperation(value = "Check the prize of a Bet for a draw.",
+            notes = "The bet and the draw id is sended. The prize is returned",
+            response = PrizeInfo.class
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Information with the prize for this bet is returned"),
+            @ApiResponse(code = 404, message = "No Chispazo found for the Id passed as argument"),
+    })
     public Response calculatePrizeInfo(@PathParam("drawId") Long drawId,
                                         ChispazoBet bet){
         throw new UnsupportedOperationException("To be developed following the tests in ChispazoResourceTest");
